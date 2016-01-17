19162a296201b0acf060a70a0936daf345aacbfa
Ilari Stenroth <istenrot@users.noreply.github.com>
2016-01-17 08:03:30 +0200

    Set StrictHostKeyChecking=no for rsync

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Building Nginx for RHEL/CentOS/SL 6...."
 
 vagrant up
-rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
-rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
 vagrant destroy
 
