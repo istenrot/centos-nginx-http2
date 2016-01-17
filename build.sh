ad5ec2a5035a480a1de7c23758e9aa1705eb2c1c
Ilari Stenroth <istenrot@users.noreply.github.com>
2016-01-17 07:13:20 +0200

    Nginx 1.9.9 and OpenSSL 1.0.2e EL7 Vagrantfile and Ansible playbook

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Building Nginx for RHEL/CentOS/SL 7...."
+
+vagrant up
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
+vagrant destroy
+
