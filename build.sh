d73ac92cc52879b3fc967351921810e80c7587e8
Ilari Stenroth <istenrot@users.noreply.github.com>
2016-01-17 20:34:47 +0200

    Add CloudFlare CHACHA20-POLY1305 patch for OpenSSL, disable insecure ciphers/features in OpenSSL

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Building Nginx for RHEL/CentOS/SL 7...."
 
 vagrant up
-rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
-rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
 vagrant destroy
 
