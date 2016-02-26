2e312b8fa4212fc562dd6927dd9825fe17e03ca8
Ilari Stenroth <istenrot@users.noreply.github.com>
2016-02-26 02:44:41 +0200

    Nginx 1.9.12, add dynamic pagespeed, spec generator

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 echo "Building Nginx for RHEL/CentOS/SL 6...."
 
-vagrant up
+vagrant up --no-provision
+vagrant provision --provision-with build
 rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
 rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
 vagrant destroy
---
+++ generate-spec.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Generating Nginx RPM spec for RHEL/CentOS/SL 6...."
+
+vagrant up --no-provision
+vagrant provision --provision-with spec
+rsync -e "ssh -p2222 -i $HOME/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" vagrant@127.0.0.1:rpmbuild/SPECS/* specs/
+vagrant destroy
+
