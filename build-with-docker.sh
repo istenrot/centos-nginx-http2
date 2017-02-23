cf070343d64212ee3d6eb85bf42bdf6efe1523c8
Ilari Stenroth <ilari.stenroth@iki.fi>
2017-02-23 02:56:20 +0200

    Add ngx_http_geoip_module as a dynamic module, fixes to Docker build, make a complete rebuildable src RPM

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,11 +3,11 @@
 echo "Building Nginx for RHEL/CentOS/SL 6 with Docker...."
 
 docker pull centos:6
-docker run --sysctl net.ipv6.conf.all.disable_ipv6=1 --name centos6_nginx_builder -it -d centos:6 /bin/bash || docker restart centos6_nginx_builder
+docker run --net=host --name centos6_nginx_builder -it -d centos:6 /bin/bash || docker restart centos6_nginx_builder
 ansible-playbook -c docker -i centos6_nginx_builder, -u root build-nginx-openssl.yml || exit 1
-docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.centos.exove.x86_64.rpm packages/
-docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-debuginfo-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.centos.exove.x86_64.rpm packages/
-docker cp centos6_nginx_builder:/root/rpmbuild/SRPMS/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.centos.exove.src.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-debuginfo-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/SRPMS/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.src.rpm packages/
 docker stop centos6_nginx_builder
 docker rm centos6_nginx_builder
 
