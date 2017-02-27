05f47d57fe6f7305aef3aa2657a87e1fe21d9fec
Ilari Stenroth <ilari.stenroth@iki.fi>
2017-02-27 21:30:01 +0200

    Build GeoIP module as a separate rpm package

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -2,12 +2,21 @@
 
 echo "Building Nginx for RHEL/CentOS/SL 6 with Docker...."
 
+ngx_ver=$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)
+rpm_release=$(sed -e 's/rpm_release: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)
 docker pull centos:6
 docker run --net=host --name centos6_nginx_builder -it -d centos:6 /bin/bash || docker restart centos6_nginx_builder
+
 ansible-playbook -c docker -i centos6_nginx_builder, -u root build-nginx-openssl.yml || exit 1
-docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.x86_64.rpm packages/
-docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-debuginfo-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.x86_64.rpm packages/
-docker cp centos6_nginx_builder:/root/rpmbuild/SRPMS/nginx-$(sed -e 's/nginx_ver: "\(.*\)-1"/\1/'  -e 'tx' -e 'd' -e ':x' variables.yml)-2.el6.exove.src.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-$ngx_ver-$rpm_release.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-debuginfo-$ngx_ver-$rpm_release.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/SRPMS/nginx-$ngx_ver-$rpm_release.el6.exove.src.rpm packages/
+
+ansible-playbook -c docker -i centos6_nginx_builder, -u root build-nginx-module-geoip.yml || exit 1
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-module-geoip-$ngx_ver-$rpm_release.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/RPMS/x86_64/nginx-module-geoip-debuginfo-$ngx_ver-$rpm_release.el6.exove.x86_64.rpm packages/
+docker cp centos6_nginx_builder:/root/rpmbuild/SRPMS/nginx-module-geoip-$ngx_ver-$rpm_release.el6.exove.src.rpm packages/
+
 docker stop centos6_nginx_builder
 docker rm centos6_nginx_builder
 
--- build-with-vagrant.sh
+++ build-with-vagrant.sh
@@ -4,7 +4,10 @@ echo "Building Nginx for RHEL/CentOS/SL 6...."
 
 vagrant up --no-provision
 SSHARGS=`vagrant ssh-config | awk 'NF && !/^Host / {print " -o "$1"="$2}'`
-vagrant provision --provision-with build
+vagrant provision --provision-with build-nginx-openssl
+rsync -e "ssh $SSHARGS" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
+rsync -e "ssh $SSHARGS" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
+vagrant provision --provision-with build-nginx-module-geoip
 rsync -e "ssh $SSHARGS" vagrant@127.0.0.1:rpmbuild/RPMS/x86_64/* packages/
 rsync -e "ssh $SSHARGS" vagrant@127.0.0.1:rpmbuild/SRPMS/* packages/
 vagrant destroy
